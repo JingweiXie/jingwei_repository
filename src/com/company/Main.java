3bfa5433bf1568995a99c3b36240c5e89e056887
xiejingwei <xiejingwei@tele-hot.com>
2020-01-08 13:34:24 +0800

    branch changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,9 @@ package com.company;
 
 public class Main {
 
+    int age;
+    String address;
+
     public static void main(String[] args) {
         System.out.println("hello tomorrow!");
     }
